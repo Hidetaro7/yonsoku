4290606ebf14c078a046f335fb26530fc71771c5
Hidetaro7 <hidetaro7@gmail.com>
2020-10-24 20:31:01 +0900

    3rd commit

--- src/script/index.js
+++ src/script/index.js
@@ -2,6 +2,6 @@
 const figs = document.querySelector(".feature-image");
 for(var i = 0; i< 100; i++) {
   const img = document.createElement("img");
-  img.src = "/image/fig-sample.jpg";
+  img.src = "./image/fig-sample.jpg";
   figs.appendChild(img)
 }
