3089555fd94e43f4a9e8a8ae6932307a66786710
Hidetaro7 <hidetaro7@gmail.com>
2020-10-24 20:06:23 +0900

    first commit

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+
+const figs = document.querySelector(".feature-image");
+for(var i = 0; i< 100; i++) {
+  const img = document.createElement("img");
+  img.src = "/image/fig-sample.jpg";
+  figs.appendChild(img)
+}
